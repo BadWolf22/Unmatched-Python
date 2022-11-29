449ab4ea112abdf98db4aa0168b1ab44ed9a3c0e
BadWolf22 <matthewpostel02@gmail.com>
2022-11-29 14:21:15 -0600

    Implemented menus using Pygame-Menu

--- server.py
+++ server.py
@@ -2,25 +2,24 @@ import eventlet
 import socketio
 import json
 
-DEBUG = False
+DEBUG = True
 
 sio = socketio.Server()
 app = socketio.WSGIApp(sio)
 
-publicRooms = list()
-privateRooms = list()
+roomList = list()
 
 clients = dict()
 
 @sio.event
 def connect(sid, environ):
     if DEBUG: print('connect ', sid)
-    sio.emit("roomlist", json.dumps(publicRooms), room=sid)
+    clients[sid] = dict()
 
 @sio.event
 def disconnect(sid):
     if DEBUG: print('disconnect ', sid)
-
+    del clients[sid]
 
 
 @sio.on("message")
@@ -32,14 +31,12 @@ def my_message(sid, data):
         if DEBUG: print("message attempt while not in a room")
 
 @sio.on("joinroom")
-def join_room(sid, name, public):
-    if DEBUG: print(sid, name, public)
-    clients[sid] = dict()
+def join_room(sid, name):
+    if DEBUG: print(sid, name)
+    if name not in roomList:
+        roomList.append(name)
     clients[sid]["room"] = name
     sio.enter_room(sid, name)
-    if name not in publicRooms or name not in privateRooms:
-        if public: publicRooms.append(name)
-        else: privateRooms.append(name)
 
 if __name__ == '__main__':
     eventlet.wsgi.server(eventlet.listen(('', 5000)), app)
